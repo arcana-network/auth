2a4aacf3ab9c2e23a6368f6206e412394a46af36
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-01 17:07:37 +0530

    create interface for widget theme config

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,6 +3,16 @@ export interface iframeWrapperParams {
   network: string;
 }
 
+export interface widgetThemeConfig {
+  assets: {
+    logo_vertical_light: string,
+    logo_vertical_dark: string,
+    logo_horizontal_light: string,
+    logo_horizontal_dark: string,
+  },
+  theme: string
+}
+
 export interface IConnectionMethods {
   isLoggedIn: () => Promise<boolean>;
   triggerLogin: (t: string) => Promise<void>;
