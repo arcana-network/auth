fe0b0626e4ecd39a4cfd415a91cf2ca810a5ed78
makylfang <makyl@newfang.io>
2022-05-28 18:59:47 +0400

    Added function to documentation

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const esbuild = require('esbuild')
 const plugin = require('node-stdlib-browser/helpers/esbuild/plugin')
 const stdLibBrowser = require('node-stdlib-browser')
 ;(async () => {
-  console.time('Build time')
+  console.time('IIFE Build time')
   const result = await esbuild
     .build({
       entryPoints: ['src/index.ts'],
@@ -26,7 +26,40 @@ const stdLibBrowser = require('node-stdlib-browser')
       console.log({ error: e })
       process.exit(0)
     })
-  console.timeEnd('Build time')
+  console.timeEnd('IIFE Build time')
+  if (result.errors && result.errors.length) {
+    console.log({ errors: result.errors })
+  } else {
+    console.info('Compiled successfully!')
+  }
+  if (result.warnings && result.warnings.length) {
+    console.log({ warnings: result.warnings })
+  }
+})()
+;(async () => {
+  console.time('ESM Build time')
+  const result = await esbuild
+    .build({
+      entryPoints: ['src/index.ts'],
+      bundle: true,
+      target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
+      globalName: 'arcana.wallet',
+      inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+      plugins: [plugin(stdLibBrowser)],
+      define: {
+        global: 'window',
+        process: 'process',
+        Buffer: 'Buffer',
+      },
+      format: 'esm',
+      outfile: 'dist/standalone/wallet.esm.js',
+      minify: true,
+    })
+    .catch((e) => {
+      console.log({ error: e })
+      process.exit(0)
+    })
+  console.timeEnd('ESM Build time')
   if (result.errors && result.errors.length) {
     console.log({ errors: result.errors })
   } else {
