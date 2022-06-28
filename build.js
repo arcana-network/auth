3c076d25ead0b7d402b79c01d06898cf850a766b
makylfang <makyl@newfang.io>
2022-06-28 12:06:27 +0400

    AR 3198 esm error (#57)
    * Added exports to package.json
    
    * Removed esm build from module key in package.json
    
    * Added rimraf to dependency and updated version

--- build.js
+++ build.js
@@ -37,36 +37,3 @@ const stdLibBrowser = require('node-stdlib-browser')
     console.log({ warnings: result.warnings })
   }
 })()
-;(async () => {
-  console.time('ESM Build time')
-  const result = await esbuild
-    .build({
-      entryPoints: ['src/index.ts'],
-      bundle: true,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
-      globalName: 'arcana.auth',
-      inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-      plugins: [plugin(stdLibBrowser)],
-      define: {
-        global: 'window',
-        process: 'process',
-        Buffer: 'Buffer',
-      },
-      format: 'esm',
-      outfile: 'dist/standalone/auth.esm.js',
-      minify: true,
-    })
-    .catch((e) => {
-      console.log({ error: e })
-      process.exit(0)
-    })
-  console.timeEnd('ESM Build time')
-  if (result.errors && result.errors.length) {
-    console.log({ errors: result.errors })
-  } else {
-    console.info('Compiled successfully!')
-  }
-  if (result.warnings && result.warnings.length) {
-    console.log({ warnings: result.warnings })
-  }
-})()
