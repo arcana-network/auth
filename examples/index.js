af2c900aa2404ac6e56d53a5d15f180ddce417ef
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-11-25 12:39:28 +0530

    remove array

--- examples/index.js
+++ examples/index.js
@@ -97,17 +97,15 @@ async function addToken() {
   try {
     await provider.request({
       method: 'wallet_watchAsset',
-      params: [
-        {
-          type: 'ERC20',
-          options: {
-            address: '0xB983E01458529665007fF7E0CDdeCDB74B967Eb6',
-            symbol: 'FOO',
-            decimals: 18,
-            image: 'https://foo.io/token-image.svg',
-          },
+      params: {
+        type: 'ERC20',
+        options: {
+          address: '0xB983E01458529665007fF7E0CDdeCDB74B967Eb6',
+          symbol: 'FOO',
+          decimals: 18,
+          image: 'https://foo.io/token-image.svg',
         },
-      ],
+      },
     })
   } catch (e) {
     console.log({ e })
