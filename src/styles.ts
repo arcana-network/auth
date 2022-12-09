81d9869949ffd5feddf8a7af4bb5324749977ea9
makylfang <makyl@newfang.io>
2022-12-09 12:18:58 +0400

    Added chainId to auth, updated zIndex of elements to stay above other packages

--- src/styles.ts
+++ src/styles.ts
@@ -95,7 +95,7 @@ const widgetIframeStyle = {
     boxShadow: '4px 5px 4px rgba(0, 0, 0, 0.25)',
     borderRadius: '10px',
 
-    zIndex: '100000',
+    zIndex: '2147483648',
   },
   header: {
     container: {
