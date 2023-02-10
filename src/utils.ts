8256dd9eea6b655966ac62c3ad6a70d04f0f503d
makylfang <makyl@newfang.io>
2023-02-10 16:24:56 +0400

    Make testnet as the default network

--- src/utils.ts
+++ src/utils.ts
@@ -239,7 +239,7 @@ const getCurrentUrl = () => {
 
 const getConstructorParams = (initParams?: Partial<ConstructorParams>) => {
   const p: ConstructorParams = {
-    network: 'mainnet',
+    network: 'testnet',
     debug: false,
     position: 'right',
     theme: 'dark',
