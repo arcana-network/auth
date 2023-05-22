29856807775de79db6598988432d70a4fe46f52e
Karthik Durai <iamkarthiksiddarth@gmail.com>
2023-05-22 17:29:10 +0530

    Hot Fix (#174)
    * change absolute to fixed
    
    * set z-index
    
    * set z-index to higher value

--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,11 @@
 const iFrameInitialStyle = {
   border: 'none',
-  position: 'absolute',
+  position: 'fixed',
   height: 0,
   width: 0,
   right: 0,
   bottom: 0,
+  zIndex: 2147483648,
 }
 
 export { iFrameInitialStyle }
