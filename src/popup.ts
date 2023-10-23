79bf158970b7cf02493141c2451edc22b7b2cf15
makylfang <makyl@newfang.io>
2023-10-23 08:44:59 -0400

    Added basic code for permission tab

--- src/popup.ts
+++ src/popup.ts
@@ -5,10 +5,16 @@ class Popup {
   private window: Window | null
   constructor(public url: string) {}
 
-  public open() {
+  public open(type: 'login'): Promise<unknown>
+  public open(type: 'request'): Promise<JsonRpcResponse<unknown>>
+  public open(type: 'login' | 'request' = 'login') {
     const windowFeatures = getWindowFeatures()
     this.window = window.open(this.url, '_blank', windowFeatures)
-    return this.getWindowResponse()
+    if (type == 'login') {
+      return this.getWindowResponse()
+    } else {
+      return this.requestHandler()
+    }
   }
 
   private getWindowResponse() {
@@ -37,6 +43,35 @@ class Popup {
           return resolve('done')
         } else {
           console.log('Unexpected event')
+        }
+      }
+      window.addEventListener('message', handler, false)
+    })
+  }
+
+  requestHandler = (): Promise<any> => {
+    return new Promise((resolve) => {
+      let cleanExit = false
+      const id = window.setInterval(() => {
+        try {
+          if (!cleanExit && this.window?.closed) {
+            return resolve({
+              id: 1,
+              jsonrpc: '2.0',
+              error: 'user_closed_popup',
+            })
+          }
+          // eslint-disable-next-line no-empty
+        } catch (e) {}
+      }, 500)
+      const handler = async (
+        event: MessageEvent<{ type?: 'json_rpc_response'; response: any }>
+      ) => {
+        if (event.data.type == 'json_rpc_response') {
+          cleanExit = true
+          this.clear(handler, id)
+          this.window?.close()
+          return resolve(event.data.response)
         }
       }
       window.addEventListener('message', handler, false)
