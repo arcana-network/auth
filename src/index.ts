638294895dd34508850bb7807df83daa8312d30e
makylfang <makyl@newfang.io>
2023-11-16 13:16:37 +0530

    Added new RequestPopup class

--- src/index.ts
+++ src/index.ts
@@ -342,7 +342,7 @@ class AuthProvider {
       u.searchParams.set('sessionID', session.id)
 
       const popup = new Popup(u.toString())
-      await popup.open('login')
+      await popup.open()
       await this.waitForConnect()
       return
     }
@@ -359,7 +359,7 @@ class AuthProvider {
 
   private async beginLogin(url: string): Promise<EthereumProvider> {
     const popup = new Popup(url)
-    await popup.open('login')
+    await popup.open()
     return await this.waitForConnect()
   }
 
