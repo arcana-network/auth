f22ee0558172ecc06fea3672724998bac2b84fd0
makylfang <makyl@newfang.io>
2024-01-09 16:10:25 +0400

    [Fix] ESM build changes (#211)
    * intial code for esm build
    
    * Little adjustment
    
    ---------
    
    Co-authored-by: mmjee <root@git.maharshi.ninja>

--- rollup.es.config.js
+++ rollup.es.config.js
@@ -3,8 +3,17 @@ import baseConfig from './rollup.base.config'
 export default {
   ...baseConfig,
   output: {
-    file: 'dist/standalone/auth.esm.js',
-    format: 'es',
+    dir: 'dist',
+    // file: 'dist/standalone/auth.esm.js',
+    format: 'esm',
     compact: true,
+    chunkFileNames: '[name].chunk.js',
+    manualChunks: function (id) {
+      if (id.includes('@solana/web3.js/lib')) {
+        return 'solana'
+      } else if (id.includes('/node_modules/')) {
+        return 'vendor'
+      }
+    },
   },
 }
